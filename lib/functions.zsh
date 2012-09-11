b8c1121b71725152a9d505eeee1e0e3bb37adb1b
Sebastian Celis <sebastian@sebastiancelis.com>
2012-09-11 15:38:27 -0500

    Add prompt_remote_hostname function.

--- lib/functions.zsh
+++ lib/functions.zsh
@@ -10,6 +10,12 @@ function upgrade_oh_my_zsh() {
   /usr/bin/env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
 }
 
+function prompt_remote_hostname {
+  if [ -n "$SSH_TTY" ]; then
+    echo "$ZSH_THEME_REMOTE_HOSTNAME_PREFIX%m$ZSH_THEME_REMOTE_HOSTNAME_SUFFIX"
+  fi
+}
+
 function take() {
   mkdir -p $1
   cd $1
